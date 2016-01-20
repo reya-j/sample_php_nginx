a930d833dd447dfd002f2c344e06001450530e51
chetantarale <chetan@shippable.com>
2016-01-20 11:38:38 +0530

    Update ExampleTest.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,7 +3,7 @@ require_once('Example.php');
 class ExampleTest extends PHPUnit_Framework_TestCase {
   public function testExample() {
   	$exmp = new Example();
-    $this->assertEquals($exmp->test(), 'shippable') == 1, true);
+    $this->assertEquals($exmp->test(), 'shippable');
   }
 }
 ?>
