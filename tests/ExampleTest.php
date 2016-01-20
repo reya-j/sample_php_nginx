e7a50b1e62905a4388ef9b722443698552bafefe
chetantarale <chetan@shippable.com>
2016-01-20 12:47:39 +0530

    using substr

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,7 +3,7 @@ require_once('Example.php');
 class ExampleTest extends PHPUnit_Framework_TestCase {
   public function testExample() {
   	$exmp = new Example();
-    $this->assertEquals($exmp->test(), 'shippable');
+    $this->assertEquals(substr($exmp->test(), 'shippable'));
   }
 }
 ?>
